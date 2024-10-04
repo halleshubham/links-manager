7abf3276f62b159c186d2ff3a1cb6d25a3d32167
halleshubham <halleshubham@gmail.com>
2024-10-05 01:08:14 +0530

    Added top level menu item - Your Managers

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,17 +1,25 @@
-// src/NavBar.tsx
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Menu, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorElManagers, setAnchorElManagers] = React.useState<null | HTMLElement>(null);
+  const [anchorElConversions, setAnchorElConversions] = React.useState<null | HTMLElement>(null);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleManagersClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElManagers(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleManagersClose = () => {
+    setAnchorElManagers(null);
+  };
+
+  const handleConversionsClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElConversions(event.currentTarget);
+  };
+
+  const handleConversionsClose = () => {
+    setAnchorElConversions(null);
   };
 
   return (
@@ -21,9 +29,21 @@ const NavBar: React.FC = () => {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Utilify
         </Typography>
-        <Button color="inherit" component={Link} to="/links-manager">
-          Links Manager
+        <Button color="inherit" onClick={handleManagersClick}>
+          Your Managers
         </Button>
+        <Menu
+          anchorEl={anchorElManagers}
+          open={Boolean(anchorElManagers)}
+          onClose={handleManagersClose}
+        >
+          <MenuItem component={Link} to="/links-manager" onClick={handleManagersClose}>
+            Links Manager
+          </MenuItem>
+          <MenuItem component={Link} to="/tasks-manager" onClick={handleManagersClose}>
+            Tasks Manager
+          </MenuItem>
+        </Menu>
         <Button color="inherit" component={Link} to="/image-compressor">
           Image Compressor
         </Button>
@@ -33,27 +53,24 @@ const NavBar: React.FC = () => {
         <Button color="inherit" component={Link} to="/background-remover">
           Background Remover
         </Button>
-        <Button color="inherit" onClick={handleClick}>
+        <Button color="inherit" onClick={handleConversionsClick}>
           File Conversions
         </Button>
         <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
+          anchorEl={anchorElConversions}
+          open={Boolean(anchorElConversions)}
+          onClose={handleConversionsClose}
         >
-          <MenuItem component={Link} to="/csv-to-excel" onClick={handleClose}>
+          <MenuItem component={Link} to="/csv-to-excel" onClick={handleConversionsClose}>
             CSV to Excel
           </MenuItem>
-          <MenuItem component={Link} to="/excel-to-csv" onClick={handleClose}>
+          <MenuItem component={Link} to="/excel-to-csv" onClick={handleConversionsClose}>
             Excel to CSV
           </MenuItem>
-          <MenuItem component={Link} to="/csv-to-vcf" onClick={handleClose}>
+          <MenuItem component={Link} to="/csv-to-vcf" onClick={handleConversionsClose}>
             CSV to VCF
           </MenuItem>
         </Menu>
-        <Button color="inherit" component={Link} to="/tasks-manager">
-          Tasks Manager
-        </Button>
       </Toolbar>
     </AppBar>
   );
